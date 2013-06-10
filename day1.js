3554de186e0b325dd465978bfd560b52902ddcaf
Chris Terman <cjt@mit.edu>
2013-06-10 10:42:03 -0400

    changes this morning

---
+++ day1.js
@@ -0,0 +1,9 @@
+/*
+  calculate: evaluate the value of an arithmetic expression
+*/
+function calculate() {
+    var input = $('#text1:first');  // get input field from DOM
+    var val = input.val();
+    var output = $('#text1_out:first');
+    output.text(val);
+}
