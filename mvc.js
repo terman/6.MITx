08d70b54c0eb0a4b38a7cce95299a9bfa4e121b7
Chris Terman <cjt@mit.edu>
2013-06-18 08:34:40 -0400

    add comments to mvc.js

--- mvc.js
+++ mvc.js
@@ -1,16 +1,20 @@
 var counter = (function () {
+    // implement Observable interface
     function Observers() {
 	var observers = [];
 
+	// add a new callback function to the list of observers
 	function add(observer) {
 	    observers.push(observer);
 	}
 
+	// remove observer from list
 	function remove(observer) {
 	    var i = observers.indexOf(observer);
 	    if (i != -1) observers.splice(i,1);
 	}
 
+	// invoke each callback with message argument
 	function notify(message) {
 	    for (var i = 0; i < observers.length; i += 1) {
 		observers[i](message);
@@ -25,7 +29,7 @@ var counter = (function () {
     //   add_observer(observer)   // call observer("increment") when counter increments
     //   increment()		  // increment counter value
     //   value = get_count()      // returns current counter value
-    function Model() {
+    function CounterModel() {
 	var count = 0;
 	var observers = Observers();
 
@@ -43,7 +47,7 @@ var counter = (function () {
 
     // exports:
     //   increment()		// call to process increment request
-    function Controller(model) {
+    function CounterController(model) {
 	function increment() {
 	    model.increment();
 	}
@@ -54,7 +58,7 @@ var counter = (function () {
 
     // exports:
     //   none
-    function View(div,model,controller,color) {
+    function CounterView(div,model,controller,color) {
 	var display = $('<div class="view">The current value of the counter is <span>0</span>.</div>');
 	var counter_value = display.find('span');
 	display.css("background-color",color || "white");
@@ -77,10 +81,10 @@ var counter = (function () {
 
 
     function setup(div) {
-	var model = Model();
-	var controller = Controller(model);
-	var view = View(div,model,controller);
-	var view2 = View(div,model,controller,'#E0FFE0');
+	var model = CounterModel();
+	var controller = CounterController(model);
+	var view = CounterView(div,model,controller);
+	var view2 = CounterView(div,model,controller,'#E0FFE0');
 
 	var button = $('<button>Increment</button>');
 	button.on("click",controller.increment);
