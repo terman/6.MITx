6004d26e15fc04a98fb0c5ff2201935d8701a189
Chris Terman <cjt@mit.edu>
2013-06-19 09:27:58 -0400

    more knapsack

--- knapsack.js
+++ knapsack.js
@@ -59,26 +59,31 @@ var knapsack = (function () {
 		knapsack_value -= item.value;
 	    }
 	    observers.notify("item moved");
+	    return true;
 	}
 
 	function get_items() { return items; }
-	function knapsack_value() { return knapsack_value; }
-	function knapsack_weight() { return knapsack_value; }
+	function get_value() { return knapsack_value; }
+	function get_weight() { return knapsack_weight; }
 
 	return {add_observer: observers.add,
 		reset: reset,
 		move_item: move_item,
 		get_items: get_items,
-		knapsack_weight: knapsack_weight,
-		knapsack_value: knapsack_value };
+		get_weight: get_weight,
+		get_value: get_value };
     }
 
     // exports:
+    //   add_observer()
     //   increment()		// call to process increment request
     function Controller(model) {
+	var observers = Observers();
+
 	function item_clicked(item) {
 	    if (!model.move_item(item)) {
 		// oops, knapsack overflow!
+		observers.notify("knapsack overflow");
 	    }
 	}
 
@@ -86,20 +91,44 @@ var knapsack = (function () {
 	    model.reset();
 	}
 
-	return { item_clicked: item_clicked, reset: reset }
+	return { add_observer: observers.add, item_clicked: item_clicked, reset: reset }
     }
 
 
     // exports:
     //   none
     function View(div,model,controller) {
+	// add alert, initially transparent (and hence invisible)
+	var alert = $('<div class="alert">Oops! Knapsack capacity exceeded</div>');
+	div.append(alert);
+
+	// add captions to each image
+	var items = model.get_items();
+	for (var item_name in items) {
+	    var item = items[item_name];
+	    var idiv = $('<div class="item"></div>');
+	    idiv.append(item.image);
+	    idiv.append($('<br>'));
+	    idiv.append('$'+item.value+', '+item.weight+'kg');
+	    item.image[0].item = item;    // provide info for click hanlder
+	    item.image = idiv;
+	}
+
+	// attach click handler to top-most div, since moving DOM objects on and off
+	// page clears any event bindings
+	div.on("click",function(event) {
+		controller.item_clicked(event.target.item);
+	    });
+
 	// set up house display
-	var house = $('<div class="location"><div class="header">House</div><div class="items"></div><div class="footer">Click item to move to knapsack</div></div>');
+	var house = $('<div class="location"><div class="header"><img src="house.png"></div><div class="items"></div><div class="footer">Click item to move to knapsack</div></div>');
 	var house_items = house.find('.items');
 
 	// set up knapsack display
-	var knapsack = $('<div class="location"><div class="header">Knapsack</div><div class="items"></div><div class="footer">Click item to move to house</div></div>');
+	var knapsack = $('<div class="location"><div class="header"><img src="burglar.png">($<span id="value">0</span>, <span id=weight>0</span>kg)</div><div class="items"></div><div class="footer">Click item to move to house</div></div>');
 	var knapsack_items = knapsack.find('.items');
+	var weight = knapsack.find('#weight');
+	var value = knapsack.find('#value');
 
 	div.append(house,knapsack);
 	position_items();
@@ -117,13 +146,24 @@ var knapsack = (function () {
 		if (item.location == "house") house_items.append(item.image);
 		else knapsack_items.append(item.image);
 	    }
+	    weight.text(model.get_weight());
+	    value.text(model.get_value());
 	}
 
-	// handle notifications from the model
+	// make alter visible for 3 seconds
+	function show_alert() {
+	    alert.animate({opacity: 1},1000);
+	    alert.animate({opacity: 1},1000); // pause
+	    alert.animate({opacity: 0},1000);
+	}
+
+	// handle notifications from the model, controller
 	function notify(message) {
 	    if (message == "item moved") position_items();
+	    if (message == "knapsack overflow") show_alert();
 	}
 	model.add_observer(notify);
+	controller.add_observer(notify);
 
 	return {};
     }
@@ -137,22 +177,14 @@ var knapsack = (function () {
 		var name = image.attr('data-name');
 		var value = parseFloat(image.attr('data-value'));
 		var weight = parseFloat(image.attr('data-weight'));
-		items[name] = {image: image, name: name, value: value, weight: weight};
-		image.addClass('item');  // let css set image dimensions, etc.
+		var item = {image: image, name: name, value: value, weight: weight};
+		items[name] = item;
 	    });
 	div.empty();  // clear it out!
 
 	var model = Model(items,parseFloat(div.attr('data-maxweight')));   // max weight in knapsack: 20
 	var controller = Controller(model);
 	var view = View(div,model,controller);
-
-	// clicking on image invokes controller
-	for (var item_name in items) {
-	    var item = items[item_name];
-	    item.image.on("click",
-			  function (event) { console.log('img click');}, // controller.item_clicked(event.data); },
-			  item);
-	}
 
 	model.reset();
     }
